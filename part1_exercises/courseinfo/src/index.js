aee6e7e17a97c0fe84136c3ff5352f73c953b55c
Martin Danev <martin.danev01@gmail.com>
2019-09-26 17:50:50 +0200

    Fixed object rendering

--- part1_exercises/courseinfo/src/index.js
+++ part1_exercises/courseinfo/src/index.js
@@ -36,29 +36,39 @@ const Total = (props) => {
 }
 
 const App = () => {
-    const course = "Half Stack application development"
-  //  const part1 = "Fundamentals of React"
-   // const exercises1 = 10
-   // const part2 = "Using props to pass data"
-   // const exercises2 = 7
-    // const part3 = "State of a component"
-   // const exercises3 = 14
     const courses = {
         "Fundamentals of React": 10,
         "Using props to pass data": 7,
         "State of a component": 14
     }
-    var total = 0
-    Object.entries(courses).map(([part, exercises]) => {
-        total += exercises;
+
+    const courseObj = {
+        name: "Half stack application development",
+        parts: [
+            {
+                name: "Fundamentals of React",
+                exercises: 10
+            },
+            {
+                name: "Using props to pass data",
+                exercises: 7
+            },
+            {
+                name: "State of a component",
+                exercises: 14
+            }
+        ]
+    }
+    let total = 0
+    Object.entries(courseObj.parts).map(([_, exercises]) => {
+        total += exercises.exercises;
     })
-   // const total = courses.reduce((prev, next) => prev + next.)
     return (
         <div>
-            <Header course = {course} />
-            {Object.entries(courses).map(([name, exercises]) => {
-                return (
-                    <Content part = {name} exercises = {exercises}/>
+            <Header course = {courseObj.name} />
+            {Object.entries(courseObj.parts).map(([n, e]) => {
+                return(
+                <Content part = {e.name} exercises = {e.exercises} />
                 )
             })}
             <Total exercises = {total}/>
