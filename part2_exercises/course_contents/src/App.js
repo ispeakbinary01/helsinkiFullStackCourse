c9ace11f2bd433860447df446ab84680ea9e76e9
Martin Danev <martin.danev01@gmail.com>
2019-10-01 03:26:45 +0200

    part2 step7 and 8 completed

--- part2_exercises/course_contents/src/App.js
+++ part2_exercises/course_contents/src/App.js
@@ -2,44 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Course from './components/Course'
 
-const App = () => {
-    const course = {
-        name: "Half Stack application development",
-        parts: [
-            {
-                name: "Fundamentals of React",
-                exercises: 10,
-                id: 1
-            },
-            {
-                name: "Using props to pass data",
-                exercises: 7,
-                id: 2
-            },
-            {
-                name: "State of a component",
-                exercises: 14,
-                id: 3
-            },
-            {
-                name: "Redux",
-                exercises: 11,
-                id: 4
-            }
-        ]
-    }
+const App = ({ courses }) => {
 
-    return (
-        <div>
-            <h1>{course.name}</h1>
-            {course.parts.map(course => 
+     const sum = courses.parts.reduce((totalEx, course) => totalEx + course.exercises, 0)
+     return (
+         <div>
+            <h1>{courses.name}</h1>
+            {courses.parts.map(course => 
                 <Course 
-                    key = {course.id}
-                    course = {course}
+                key = {course.id}
+                course = {course}
                 />
                 )}
+            <p>Total exercises: {sum}</p>
         </div>
     )
 }
+    
 
 export default App
--- part2_exercises/course_contents/src/index.js
+++ part2_exercises/course_contents/src/index.js
@@ -3,9 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const course = {
+    name: "Half Stack application development",
+    parts: [
+        {
+            name: "Fundamentals of React",
+            exercises: 10,
+            id: 1
+        },
+        {
+            name: "Using props to pass data",
+            exercises: 7,
+            id: 2
+        },
+        {
+            name: "State of a component",
+            exercises: 14,
+            id: 3
+        },
+        {
+            name: "Redux",
+            exercises: 11,
+            id: 4
+        }
+    ]
+}
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App courses = {course} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
